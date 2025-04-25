385e16f43138238eca678ab2555e693f3e707457
Davi <davi.crdm@gmail.com>
2025-04-25 15:17:58 -0300

    adicionando novas funcoes na -ip

--- funcoes.c
+++ funcoes.c
@@ -6,12 +6,19 @@
 #include <fcntl.h> // o que faz
 #include "funcoes.h"
 
-void inserir_sem_compressao(char *archive, char **arquivos, int n){
+void inserir_sem_compressao(char *archive, char **arquivos, int n){ //n é numero de arquivos
 
     FILE *fp_archive = fopen(archive, "ab+"); //ab+ é leitura e escrita no final do arquivo
 
     for(int i; i < n; i++){
 
-        FILE *fp_membro = fopen(arquivos[i], "rb");
+        FILE *fp_membro = fopen(arquivos[i], "rb"); //como é feita conexao nomearquivo e arquivo?
+
+        fseek(fp_membro, 0, SEEK_END);
+        long int tamanho = ftell(fp_membro);
+        fseek(fp_membro, 0, SEEK_SET); //volta o ponteiro para o inicio do arquivo
+        char *buffer = malloc(tamanho);
+        fread(buffer, 1, tamanho, fp_membro);
+
     }
 }
