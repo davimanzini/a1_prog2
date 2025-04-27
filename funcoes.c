cfb758e05ba3da4e272594911ae819962f5eb726
Davi <davi.crdm@gmail.com>
2025-04-27 19:54:38 -0300

    continuacao funcao -ip

--- funcoes.c
+++ funcoes.c
@@ -10,15 +10,17 @@ void inserir_sem_compressao(char *archive, char **arquivos, int n){ //n é numer
 
     FILE *fp_archive = fopen(archive, "ab+"); //ab+ é leitura e escrita no final do arquivo
 
-    for(int i; i < n; i++){
+    for(int i = 0; i < n; i++){
 
         FILE *fp_membro = fopen(arquivos[i], "rb"); //como é feita conexao nomearquivo e arquivo?
 
         fseek(fp_membro, 0, SEEK_END);
         long int tamanho = ftell(fp_membro);
         fseek(fp_membro, 0, SEEK_SET); //volta o ponteiro para o inicio do arquivo
-        char *buffer = malloc(tamanho);
-        fread(buffer, 1, tamanho, fp_membro);
-
+        char *buffer = malloc(tamanho); //reserva espaço temp. na RAM
+        fread(buffer, 1, tamanho, fp_membro); //le do membro pro buffer
+        fwrite(buffer, 1, tamanho, fp_archive); //escreve do buffer pro archive
+        fclose(fp_membro);
+        free(buffer);
     }
 }
