58e90839f076f81075ad5d04b74e346942f29548
Davi <davi.crdm@gmail.com>
2025-05-03 11:55:09 -0300

    ajustando header

--- funcoes.h
+++ funcoes.h
@@ -21,6 +21,6 @@ void inserir_sem_compressao(char *archive, char **arquivos, int n);
 
 void lista_informacoes(char *archive);
 
-void remove_arquivos(char *archive, char **arquivos);
+void remove_arquivos(char *archive, char **arquivos, int n);
 
 
